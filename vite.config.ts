397f0b77f7b914b2006646f72b44176c0a509b0b
bon2hak <bon2beaking2@gmail.com>
2026-08-23 10:36:23 +0700

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/daisusohocduong/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
